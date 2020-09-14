b8682e902ca170b6ae50d3023532235f45874e3f
Anuja Thorat <anuja.thorat7395@gmail.com>
2020-09-14 23:26:14 +0530

    Added Products app and inserted nav bar for complete project

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('products/', include('products.urls')),
+    #path('products/products', include('products.urls'))
 
 ]
 
